0b0750b1a3e2d96c372a8a2f2dbc9fdbafbfcaa6
Daniel Souza <danielrsouza10@gmail.com>
2024-05-13 18:40:09 -0300

    criadas as classes de personagem e raca

---
+++ Cod3rsGrowth.Dominio/Personagem.cs
@@ -0,0 +1,23 @@
+﻿namespace Cod3rsGrowth.Dominio
+{
+    public class Personagem
+    {
+        private int id;
+        private string nome;
+        private int idRaca;
+
+        enum nomeClasse
+        {
+            Arqueiro,
+            Guerreiro,
+            Mago,
+            Ladrao,
+            Curandeiro
+        };
+
+        private decimal peso;
+        private decimal altura;
+        private DateTime dataDeRegistro;
+
+    }
+}
---
+++ Cod3rsGrowth.Dominio/Raca.cs
@@ -0,0 +1,7 @@
+namespace Cod3rsGrowth.Dominio;
+
+public class Raca
+{
+    private int id;
+    private string nome;
+}
